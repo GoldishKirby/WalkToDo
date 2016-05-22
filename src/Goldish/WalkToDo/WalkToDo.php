1f88c3ee6ae8d5fd972890b213d4807cae31a2a1
Kirby <goldishkirby@mailinator.com>
2016-05-23 07:49:23 +0800

    Update WalkToDo.php

--- src/Goldish/WalkToDo/WalkToDo.php
+++ src/Goldish/WalkToDo/WalkToDo.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace WalkToDo;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\block\Block;
+// use goldish\meow
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+
+
